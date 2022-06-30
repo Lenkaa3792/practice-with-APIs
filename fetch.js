5258265e5d0a510dcc5d7084b9b60de57677d3c6
Lenkaa3792 <allanlenkaa@gmail.com>
2022-06-30 16:09:23 +0300

    js

--- fetch.js
+++ fetch.js
@@ -1,8 +1,9 @@
 document.getElementById( 'button' )
 document.getElementById("inputValue");
-document.getElementById( 'name' )
+document.getElementById( 'names' )
 document.getElementById( "desc" );
-
+document.getElementById("cloud");
+document.getElementById("wind");
 
 document.getElementById( 'button' ).addEventListener( 'click', function ()
 {
@@ -10,9 +11,27 @@ document.getElementById( 'button' ).addEventListener( 'click', function ()
         'http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value + '&appid=4540ecbc96ddcc4dae4439a49eb37509'
     )
     .then( response => response.json() )
-        .then( data => console.log( data ) )
+        .then( data => 
+        {
+            valueName = data['main'][ 'name' ]
+            valueClouds = data["clouds"]['all'];
+            valueTemp = data[ 'main' ][ 'temp' ]
+             valueWind = data["wind"]['speed'];
+
+
+
+            
+            name.innerHTML = valueName;
+            cloud.innerHTML = valueClouds;
+            temp.innerHTML = valueTemp;
+           wind.innerHTML = valueWind;
+         
+
+             console.log(data)
+        } )
+       
     
-    .catch(err=> alert('wrong city'))
+    //.catch(err=> alert('wrong city'))
 
     
 })
